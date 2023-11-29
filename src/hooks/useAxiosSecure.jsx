db283279144245fa7dcfaf71b214a4dcb4a1d470
fakeistiak <istiakahmedb100.com>
2023-11-30 00:50:02 +0600

    server live url added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 
- const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
-})
+const axiosSecure = axios.create({
+  baseURL: "https://b8a12-server-side-fakeistiak.vercel.app",
+});
 const useAxiosSecure = () => {
-
-    return axiosSecure;
-
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
