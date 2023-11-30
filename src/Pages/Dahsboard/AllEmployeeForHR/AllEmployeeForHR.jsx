3f1ee49d17ff69085061976cbdc8d8510d250f4f
fakeistiak <istiakahmedb100.com>
2023-11-30 13:09:46 +0600

    modal design completed

--- src/Pages/Dahsboard/AllEmployeeForHR/AllEmployeeForHR.jsx
+++ src/Pages/Dahsboard/AllEmployeeForHR/AllEmployeeForHR.jsx
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 import Swal from "sweetalert2";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
 const AllEmployeeForHR = () => {
-  // Modal
+   const { user } = React.useContext(AuthContext);
   const [open, setOpen] = useState(false);
+  const [payModalOpen, setPayModalOpen] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [paymentDetails, setPaymentDetails] = useState({
+    month: "",
+    year: "",
+  });
+
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
@@ -27,38 +35,62 @@ const AllEmployeeForHR = () => {
     },
   });
 
-   const handleVerified = (id) => {
-     // Show SweetAlert confirmation
-     Swal.fire({
-       title: "Are you sure?",
-       text: "Once verified, you cannot undo this action!",
-       icon: "warning",
-       showCancelButton: true,
-       confirmButtonColor: "#3085d6",
-       cancelButtonColor: "#d33",
-       confirmButtonText: "Yes, verify it!",
-     }).then((result) => {
-       if (result.isConfirmed) {
-         // User clicked "Yes," proceed with verification
-         axiosPublic
-           .patch(`/employee/${id}`)
-           .then(() => {
-             // Verification successful, you can show another success alert if needed
-             Swal.fire(
-               "Verified!",
-               "The employee has been verified.",
-               "success"
-             );
-             refetch();
-           })
-           .catch((err) => {
-             // Handle verification error
-             console.log(err);
-             Swal.fire("Error", "Failed to verify the employee.", "error");
-           });
-       }
-     });
-   };
+  const handleVerified = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Once verified, you cannot undo this action!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, verify it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic
+          .patch(`/employee/${id}`)
+          .then(() => {
+            Swal.fire(
+              "Verified!",
+              "The employee has been verified.",
+              "success"
+            );
+            refetch();
+          })
+          .catch((err) => {
+            console.log(err);
+            Swal.fire("Error", "Failed to verify the employee.", "error");
+          });
+      }
+    });
+  };
+
+  const openPayModal = (employee) => {
+    setPayModalOpen(true);
+    setSelectedEmployee(employee);
+    setPaymentDetails({
+      month: "",
+      year: "",
+    });
+  };
+
+  const closePayModal = () => {
+    setPayModalOpen(false);
+    setSelectedEmployee(null);
+    setPaymentDetails({
+      month: "",
+      year: "",
+    });
+  };
+
+  const handlePay = () => {
+    // Perform the payment process using the selectedEmployee and paymentDetails
+    // You can use axios or any other method to send a payment request to the server
+    // After successful payment, you can show a success message and update the UI as needed
+    // ...
+
+    // Close the pay modal after successful payment
+    closePayModal();
+  };
 
   return (
     <div>
@@ -79,59 +111,122 @@ const AllEmployeeForHR = () => {
             </tr>
           </thead>
           <tbody className=" text-black">
-            {allEmployee?.map((employee, id) => {
-              return (
-                <tr key={employee._id} className="bg-cyan-400 text-white">
-                  <th>{id+1}</th>
-                  <td className="lg:text-md sm:text-sm font-semibold">
-                    {" "}
-                    {employee?.name}
-                  </td>
-                  <td className="lg:text-md sm:text-sm font-semibold">
-                    {employee?.email}
-                  </td>
-                  <td className="lg:text-md sm:text-sm font-semibold">
-                    {employee?.bank_account_no}
-                  </td>
-                  <td className="lg:text-md sm:text-sm font-semibold">
-                    {" "}
-                    {employee?.salary}
-                  </td>
-                  <td>
-                    {/* ... */}
-                    <button
-                      className="px-4 py-2 ml-4 font-semibold rounded-lg bg-gray-100 hover:bg-gray-200 text-black"
-                      onClick={() => handleVerified(employee?._id)}
-                      disabled={employee.isVerified === true}
-                    >
-                      {employee.isVerified === true
-                        ? "Verified \u2705"
-                        : "Verify \u274C"}
-                    </button>
-                    {/* ... */}
-                  </td>
-                  <td>
-                    <button
-                      onClick={onOpenModal}
-                      className={`px-4 py-2 ml-4 mt-1 font-semibold rounded-lg ${
-                        employee.isVerified
-                          ? "bg-rose-500 hover:bg-rose-600"
-                          : "bg-gray-300 cursor-not-allowed"
-                      } text-white`}
-                      disabled={!employee.isVerified}
-                    >
-                      Pay
-                    </button>
-                    <Modal open={open} onClose={onCloseModal} center>
-                     
-                    </Modal>
-                  </td>
-                </tr>
-              );
-            })}
+            {allEmployee?.map((employee, id) => (
+              <tr key={employee._id} className="bg-cyan-400 text-white">
+                <th>{id + 1}</th>
+                <td className="lg:text-md sm:text-sm font-semibold">
+                  {" "}
+                  {employee?.name}
+                </td>
+                <td className="lg:text-md sm:text-sm font-semibold">
+                  {employee?.email}
+                </td>
+                <td className="lg:text-md sm:text-sm font-semibold">
+                  {employee?.bank_account_no}
+                </td>
+                <td className="lg:text-md sm:text-sm font-semibold">
+                  {" "}
+                  {employee?.salary}
+                </td>
+                <td>
+                  <button
+                    className="px-4 py-2 ml-4 font-semibold rounded-lg bg-gray-100 hover:bg-gray-200 text-black"
+                    onClick={() => handleVerified(employee?._id)}
+                    disabled={employee.isVerified === true}
+                  >
+                    {employee.isVerified === true
+                      ? "Verified \u2705"
+                      : "Verify \u274C"}
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => openPayModal(employee)}
+                    className={`px-4 py-2 ml-4 mt-1 font-semibold rounded-lg ${
+                      employee.isVerified
+                        ? "bg-rose-500 hover:bg-rose-600"
+                        : "bg-gray-300 cursor-not-allowed"
+                    } text-white`}
+                    disabled={!employee.isVerified}
+                  >
+                    Pay
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+
+      {/* Pay Modal */}
+      <Modal open={payModalOpen} onClose={closePayModal} center>
+        <div>
+          <div className="flex justify-between">
+            <div>
+              <h2 className="text-xl font-bold mb-4">Pay Employee</h2>
+              <img
+                className="w-60 h-60 object-cover rounded-lg"
+                src={user?.photoURL}
+                alt=""
+              />
+            </div>
+            <div className="pt-12 pl-20 flex-col justify-between">
+              <p className="text-xl font-semibold">
+                Employee: {selectedEmployee?.name}
+              </p>
+              <p className="text-xl font-semibold pb-8">
+                Salary: {selectedEmployee?.salary}
+              </p>
+              <select
+                required
+                className="block w-full py-2 text-gray-700 bg-white border rounded-lg px-4 my-2"
+                value={paymentDetails.month}
+                onChange={(e) =>
+                  setPaymentDetails({
+                    ...paymentDetails,
+                    month: e.target.value,
+                  })
+                }
+              >
+                <option value="" disabled>
+                  Select a month
+                </option>
+                {/* <option value="01">January</option>
+                <option value="02">February</option>
+                <option value="03">March</option>
+                <option value="04">April</option>
+                <option value="05">May</option>
+                <option value="06">June</option>
+                <option value="07">July</option>
+                <option value="08">August</option>
+                <option value="09">September</option>
+                <option value="10">October</option> */}
+                <option value="11">November</option>
+                <option value="12">December</option>
+              </select>
+              <input
+                className="block w-full py-2 text-gray-700 bg-white border rounded-lg px-4"
+                type="text"
+                placeholder="Year"
+                value={paymentDetails.year || "2023"}
+                onChange={(e) =>
+                  setPaymentDetails({
+                    ...paymentDetails,
+                    year: e.target.value,
+                  })
+                }
+              />
+            </div>
+          </div>
+
+          <button
+            onClick={handlePay}
+            className="px-4 py-2 mt-4 w-full rounded-lg bg-rose-500 hover:bg-rose-600 text-white"
+          >
+            Pay: ({selectedEmployee?.salary})
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
